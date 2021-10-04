6b91c397982d29e5686e9cc0302edb0982253327
MarvinLin <marvin_lin@cmoney.com.tw>
2021-10-04 23:56:41 +0800

    D24

---
+++ D24/ITIronMan/ITIronMan/Models/MarketType/MarketType.swift
@@ -0,0 +1,14 @@
+//
+//  MarketType.swift
+//  ITIronMan
+//
+//  Created by Marvin on 2021/9/4.
+//
+
+import Foundation
+
+enum MarketType: String {
+    case twStock = "上市"
+    case otc = "上櫃"
+    case emerging = "興櫃"
+}
---
+++ D24/ITIronMan/ITIronMan/Views/CompanyBasicInfo/CompanyBasicInfoTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CompanyBasicInfoTableViewCell.swift
+//  ITIronMan
+//
+//  Created by Marvin on 2021/9/4.
+//
+
+import UIKit
+
+class CompanyBasicInfoTableViewCell: UITableViewCell {
+
+    static let identifier = "CompanyBasicInfoTableViewCell"
+    
+    @IBOutlet weak var codeAndNameLabel: UILabel!
+    
+    @IBOutlet weak var capitalLabel: UILabel!
+}
